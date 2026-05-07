19aa9038a711a7e8e200071b62e1b8e585f01c3d
Jonas <airfryerkitten@gmail.com>
2026-05-07 10:43:17 -0400

    added tiles

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-genCard()
+function genCard(){
+    console.log("test");
+    const mainArea = document.getElementById("gridDiv");
+    for (let i = 0; i < 3; i++){
+        var row = document.createElement("div");
+        row.className = "tileRow";
+        for (let g = 0; g < 4; g++){
+            const tile = document.createElement("div");
+            tile.className = "tile"
+            tile.style.backgroundImage = "url('resources/icons/capsule.png')";
+            row.appendChild(tile);
+        }
+        mainArea.appendChild(row);
+    }
+
+}
+
+genCard();
