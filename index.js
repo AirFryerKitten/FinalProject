e0f5e63ed90d0ba4164cbcf78c7e35335cfb25da
Jonas <airfryerkitten@gmail.com>
2026-05-10 14:29:29 -0400

    You can click capsules now

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+var values = [];
+
 function genCard(){
     console.log("test");
+    let totalRuns = 0;
+    values = [];
     const mainArea = document.getElementById("gridDiv");
+    mainArea.innerHTML = ""
     for (let i = 0; i < 3; i++){
         var row = document.createElement("div");
         row.className = "tileRow";
@@ -8,11 +13,55 @@ function genCard(){
             const tile = document.createElement("div");
             tile.className = "tile"
             tile.style.backgroundImage = "url('resources/icons/capsule.png')";
+            tile.id = String(totalRuns)
+            genTile()
+            tile.onclick = function(){reveal(tile)};
             row.appendChild(tile);
+            totalRuns += 1
         }
         mainArea.appendChild(row);
     }
 
 }
 
+function genTile(){
+    const amount = Math.floor(Math.random() * 100)+1
+    values.push(amount)
+}
+
+function reveal(tile){
+    const amount = document.getElementById("amount")
+    const index = Number(tile.id);
+    const value = values[index];
+    console.log(value)
+
+    if (value < 35){//chum
+        tile.style.backgroundImage = "url('resources/icons/chum.png')";
+    }
+    else if(value < 55){//scrapper
+        tile.style.backgroundImage = "url('resources/icons/scrapper.png')";
+        amount.textContent = String(Number(amount.textContent) + 1) 
+    }
+    else if(value < 75){//flopper
+        tile.style.backgroundImage = "url('resources/icons/flopper.png')";
+        amount.textContent = String(Number(amount.textContent) + 10) 
+    }
+    else if (value < 90){//stinger
+        tile.style.backgroundImage = "url('resources/icons/stringer.png')";
+        amount.textContent = String(Number(amount.textContent) + 25) 
+    }
+    else if (value < 100){//flyfish
+        tile.style.backgroundImage = "url('resources/icons/flyfish.png')";
+        amount.textContent = String(Number(amount.textContent) + 50) 
+    }
+    else if(value == 100){//goldie
+        tile.style.backgroundImage = "url('resources/icons/goldie.png')";
+        amount.textContent = String(Number(amount.textContent) + 100) 
+    }
+
+    tile.onclick = function(){}
+
+}
+
 genCard();
+
